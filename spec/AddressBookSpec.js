9af8842c0f34c0aead0759cfca128b026b27e19a
VolantTyler <33227593+VolantTyler@users.noreply.github.com>
2018-07-07 15:44:51 -0400

    removing redundant code

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,15 +1,18 @@
 describe('Address Book', function(){
+    let addressBook,
+        thisContact;
+    
+    beforeEach(function() {
+        addressBook = new AddressBook();
+        thisContact = new Contact();
+    });
     it('should be able to add a contact', function(){
-        let addressBook = new AddressBook();
-        let thisContact = new Contact();
         addressBook.addContact(thisContact);
 
         expect(addressBook.getContact(0)).toBe(thisContact);
     });
 
     it('should be able to delete a contact', function() {
-        let addressBook = new AddressBook();
-        let thisContact = new Contact();
 
         addressBook.addContact(thisContact);
         addressBook.deleteContact(0);
